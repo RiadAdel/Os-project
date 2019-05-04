1b8a584abe2541c1b9fc3ace0b58f4ab5cc40eb9
riad adel <riad.adel22@gmail.com>
2019-05-04 12:32:01 +0200

    test1

--- NodeKeeper.py
+++ NodeKeeper.py
@@ -1,12 +1,28 @@
-import uu
+#import uu
+#uu.encode('scream.mp4', 'video.txt')
+#uu.decode('video.txt', 'video-copy.mp4')
+import zmq
+import sys
+import random
+import time
+import threading
+import os
 
-uu.encode('scream.mp4', 'video.txt')
-uu.decode('video.txt', 'video-copy.mp4')
+class NodeKeeper:
+   ip = "tcp://localhost:"
+   ports = ("1300","1400","1500")
+   files = ()
+   zmqContext = zmq.Context()
 
+   def __init__(self, clientId):
+      self.start()
 
+   def start(self):
+      print("NodeKeeper is Ready")
+      print("waiting for Service")
+      self.clientSocket.bind("tcp://*:%s" % self.port)
 
 f = open("scream.mp4", "rb")
-
 
 FileData=[]
 chunk = 1024
