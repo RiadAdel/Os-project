1a7efb052627cde870d3fd7b24c08f1c0ffaaf1c
riad adel <riad.adel22@gmail.com>
2019-05-04 18:11:55 +0200

    test2

--- Master.py
+++ Master.py
@@ -6,61 +6,32 @@ import threading
 
 class Master:
     ip = "tcp://localhost:"
-    ClientPort , DataPort , IAmAlivePort = ("9998" , "9999" , "9997")
-    nodesIps_Ports = [("tcp://localhost:" , "9991" , "9992" , "9993") , ("tcp://localhost:" , "9994" , "9995" , "9996")]
+    port = "9998"
+    nodesIps = ("tcp://localhost:","tcp://localhost:","tcp://localhost:")
+    nodesPorts = ("3941","4594","31232")
     zmqContext = zmq.Context()
-   
+    clientSocket = zmqContext.socket(zmq.REP)
 
-    def __init__(self, ip = None , ClientPort = None ,DataPort=None  , IAmAlivePort=None):
+    def __init__(self, ip = None , port = None):
         if ip != None:
               self.ip = ip
-        if ClientPort != None:
-            self.ClientPort = ClientPort
-        if DataPort  !=None:
-            self.DataPort = DataPort
-        if IAmAlivePort !=None:
-            self.IAmAlivePort = IAmAlivePort 
+        if port != None:
+            self.port = port
+
         self.start()
     
     def start(self):
         print("Server is Ready")
         print("waiting for client")
-        t = threading.Thread(target = self.ClientHandle, args = ())
-        t.start()
-        t1 = threading.Thread(target = self.DataHandle, args = ())
-        t1.start()
-        t2 = threading.Thread(target = self.AliveHandle, args = ())
-        t2.start()
-        
-    
-                 
-    def ClientHandle (self):
-        clientSocket = self.zmqContext.socket(zmq.REP)
-        clientSocket.bind("tcp://*:%s" % self.ClientPort)
-        while (True):
-            query = clientSocket.recv_pyobj()
-            print("client with ID " + str(query[0]) + "want to" + str(query[1]))
-            if query[1] == "Download":
-                     x =" "
-            elif query[1] == "Upload":
-                index = random.randint(0, len(self.nodesIps_Ports)-1)
-                l = self.nodesIps_Ports[index]
-                print(l)
-                time.sleep(10)
-                clientSocket.send_pyobj(l)
-        
-        
-        
-        
-    def DataHandle (self):
-             x =" "
-             
-
-
-
-    def AliveHandle (self):
-           x =" "
-                
+        self.clientSocket.bind("tcp://*:%s" % self.port)
+        query = self.clientSocket.recv_pyobj()
+        print("client with ID " + str(query[0]) + "want to" + str(query[1]))
+        if query[1] == "Download":
+            l = (self.nodesIps[0],self.nodesPorts[0],self.nodesIps[1],self.nodesPorts[1])
+            self.clientSocket.send_pyobj(l)
+        elif query[1] == "Upload":
+            l = (self.nodesIps[0],self.nodesPorts[0])
+            self.clientSocket.send_pyobj(l)
 
 print("server starts")
 m = Master()
