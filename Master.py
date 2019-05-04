7dc99dc959a4e4332adcf722cf80ba4b97bec5ae
Msayed97 <learnforfun13@gmail.com>
2019-05-04 12:32:41 +0200

    added upload to master

--- Master.py
+++ Master.py
@@ -6,11 +6,10 @@ import threading
 
 class Master:
     ip = "tcp://localhost:"
-    port = "9998"
-    nodesIps = ("tcp://localhost:","tcp://localhost:","tcp://localhost:")
-    nodesPorts = ("3941","4594","31232")
+    port = "9998" 
+    nodesIps_Ports = [("tcp://localhost:" , "9991" , "9992" , "9993") , ("tcp://localhost:" , "9994" , "9995" , "9996")]
     zmqContext = zmq.Context()
-    clientSocket = zmqContext.socket(zmq.REP)
+   
 
     def __init__(self, ip = None , port = None):
         if ip != None:
@@ -23,15 +22,42 @@ class Master:
     def start(self):
         print("Server is Ready")
         print("waiting for client")
-        self.clientSocket.bind("tcp://*:%s" % self.port)
-        query = self.clientSocket.recv_pyobj()
-        print("client with ID " + str(query[0]) + "want to" + str(query[1]))
-        if query[1] == "Download":
-            l = (self.nodesIps[0],self.nodesPorts[0],self.nodesIps[1],self.nodesPorts[1])
-            self.clientSocket.send_pyobj(l)
-        elif query[1] == "Upload":
-            l = (self.nodesIps[0],self.nodesPorts[0])
-            self.clientSocket.send_pyobj(l)
+        while True:
+            clientSocket = self.zmqContext.socket(zmq.REP)
+            clientSocket.bind("tcp://*:%s" % self.port)
+            query = clientSocket.recv_pyobj()
+            print("client with ID " + str(query[0]) + "want to" + str(query[1]))
+            if query[1] == "Download":
+                
+                
+                
+                l = (self.nodesIps[0],self.nodesPorts[0],self.nodesIps[1],self.nodesPorts[1])
+                #self.clientSocket.send_pyobj(l)
+            elif query[1] == "Upload":
+                t = threading.Thread(target = self.upload, args = (clientSocket))
+                t.start()
+              
+    def Download(self , ClientSocket):
+        sdfsadf
+    
+     
+
+
+
+
+    def upload (self , ClientSocket):
+        index = random.randint(0, len(self.nodesIps_Ports)-1)
+        l = self.nodesIps_Ports[index]
+        self.clientSocket.send_pyobj(l)
+        DataNodeSocket = self.zmqContext.socket(zmq.REP)
+        ip , DataPorts =  self.nodesIps_Ports[index]
+        for p in DataPorts:
+             DataNodeSocket.connect(ip+p)
+        FileName ,success , ID = clientSocket.recv_pyobj() 
+                    
+
+                        
+                
 
 print("server starts")
 m = Master()
--- NodeKeeper.py
+++ NodeKeeper.py
@@ -1,21 +1,7 @@
-import uu
-
-uu.encode('scream.mp4', 'video.txt')
-uu.decode('video.txt', 'video-copy.mp4')
+import random
 
 
 
-f = open("scream.mp4", "rb")
 
-
-FileData=[]
-chunk = 1024
-while True:
-     data = f.read(chunk)
-     if not data:
-        break
-     FileData.append(data)
-ff = open("scream2.mp4", "wb")
-for chunks in FileData:
-    ff.write(chunks)
-ff.close()     
+index = random.randint(0, 4)
+print(index)
