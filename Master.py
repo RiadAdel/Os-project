657742583290022081a9acb9b7026f30e5f99d35
Msayed97 <learnforfun13@gmail.com>
2019-05-05 14:56:44 +0200

    data node done

--- Master.py
+++ Master.py
@@ -9,16 +9,15 @@ import pymongo
 class Master:
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["mydatabase"]
-    mycol = mydb["LookUpTable"]
+    LookUpTable = mydb["LookUpTable"]
     ip = "tcp://localhost:"
-    ClientPort , DataPort , IAmAlivePort , Replication  = ("9998" , "9999" , "9997" , "9990")
-    nodesIps_Ports = [("tcp://localhost:" ,"1300","1400","1500") , ("tcp://localhost:" ,"1300","1400","1500")]
-    nodesIps_Ports_conditinos = [("tcp://localhost:","","","") , ("tcp://localhost:","","","") ]
-    zmqContext = zmq.Context()
-    clientSocket = zmqContext.socket(zmq.REP)
+    ClientPort , DataPort , IAmAlivePort , Replication  = ("9998" , "9999" , "9899" , "9990")
+    nodesIps_Ports = [("tcp://localhost:" ,"1300","1400","1500")]
+    nodesIps_Ports_conditinos = [("tcp://localhost:" ,"","","")]
+   
 
 
-    def __init__(self, ip = None , ClientPort = None ,DataPort=None  , IAmAlivePort=None , Replication = None ):
+    def __init__(self  , ClientPort=None  ,DataPort=None , IAmAlivePort=None , Replication=None , ip = None ):
         
         if ip != None:
               self.ip = ip
@@ -30,8 +29,9 @@ class Master:
             self.IAmAlivePort = IAmAlivePort 
         if Replication !=None:
             self.Replication = Replication
-            
         self.start()
+    
+    
     
     def start(self):
        
@@ -41,11 +41,11 @@ class Master:
         t.start()
         t1 = threading.Thread(target = self.DataHandle, args = ())
         t1.start()
-        t2 = threading.Thread(target = self.AliveHandle, args = ())
-        t2.start()
+        #t2 = threading.Thread(target = self.AliveHandle, args = ())
+        #t2.start()
         
         t1.join()
-        t2.join()
+        #t2.join()
         t.join()
         
     
@@ -55,24 +55,52 @@ class Master:
         clientSocket.bind("tcp://*:%s" % self.ClientPort)
         print("binded to " + self.ClientPort)
         while (True):
-            query = clientSocket.recv_pyobj()
-            print("client with ID " + str(query[0]) + "want to" + str(query[1]))
-            if query[1] == "Download":
-                     x =" "
-            elif query[1] == "Upload":
+            ID , operation , FileName = clientSocket.recv_pyobj()
+            print("client with ID " + ID+ "want to" + operation)
+            if operation == "Download":
+                
+                myquery = { "FileName": ID+FileName ,"Alive":"True"}
+                ListofDataNodes =list( self.LookUpTable.find(myquery))
+                ListofIps = []
+                for node in ListofDataNodes:
+                    ListofIps.append(node["IP"])
+                ListToSend=[]
+                count = 0
+                for p in ListofIps:
+                    for IpIndx , node in enumerate(self.nodesIps_Ports):
+                        if(node[0]==p):
+                            for portIndx in range(1,4):
+                                if(self.nodesIps_Ports_conditinos[IpIndx][portIndx]==""):
+                                     ListToSend.append(p)
+                                     ListToSend.append(node[portIndx])
+                                     self.nodesIps_Ports_conditinos[IpIndx][portIndx]=ID
+                                     count+=1
+                                if(count==6): break
+                            break
+                                    
+                        if(count==6): break            
+                            
+                    if(count==6): break        
+                TupleToSend=tuple(ListToSend)  
+                clientSocket.send_pyobj(TupleToSend)
+                     
+                
+            elif operation == "Upload":
+                
                 index = random.randint(0, len(self.nodesIps_Ports)-1)
                 l = self.nodesIps_Ports[index]
                 portsTosend=[]
                 for idx, val in enumerate(l):
                     if (self.nodesIps_Ports_conditinos[index][idx] == ""  or idx==0 ):
                          portsTosend.append(self.nodesIps_Ports[index][idx])
-              
-                print(portsTosend)
-                T = tuple(portsTosend)
+                index2 = random.randint(1, len(portsTosend)-1)
+                portsToSendFinal=[]
+                portsToSendFinal.append(portsTosend[0])
+                portsToSendFinal.append(portsTosend[index2])
+                T = tuple(portsToSendFinal)
+                print(T)
+                self.nodesIps_Ports_conditinos[index][index2]=ID
                 clientSocket.send_pyobj(T)
-        
-        
-        
         
     def DataHandle (self ):
         DataSocket = self.zmqContext.socket(zmq.REP)
@@ -81,17 +109,30 @@ class Master:
         while (True):
             ID , Ip , FileName = DataSocket.recv_pyobj()
             DataSocket.send_string("")
+            #upload
             if (FileName !=""):
-                mydict = {"ID": ID, "IP": Ip, "FileName": FileName , "Alive":"True"}
-                x = self.mycol.insert_one(mydict)
+                mydict = {"ID": ID, "IP": Ip, "FileName": ID+FileName , "Alive":"True"}
+                x = self.LookUpTable.insert_one(mydict)
                 print("updated the data base")
-                # then make it free
+                for IpIndx , node in enumerate(self.nodesIps_Ports):
+                    if(Ip==node[0]):
+                        for portIndx in range(1,4):
+                             if(self.nodesIps_Ports_conditinos[IpIndx][portIndx]==ID):
+                                 self.nodesIps_Ports_conditinos[IpIndx][portIndx]=""
+                                 break
+                        break
+                DataSocket.send_string("Done")
+            #download
             else:
-                x = ""
-                # make it free
-
-
-
+                for IpIndx , node in enumerate(self.nodesIps_Ports):
+                    if(Ip==node[0]):
+                        for portIndx in range(1,4):
+                             if(self.nodesIps_Ports_conditinos[IpIndx][portIndx]==ID):
+                                 self.nodesIps_Ports_conditinos[IpIndx][portIndx]=""
+                                 
+                        break
+                DataSocket.send_string("Done")
+                
 
     def AliveHandle (self):
         x =""
@@ -101,5 +142,11 @@ class Master:
     def ReplicationHandle (self):
         x=" "
                 
-print("server starts")
-m = Master()
+print("master starts")
+if (len(sys.argv)==4):
+    m = Master(sys.argv[1] ,sys.argv[2] ,sys.argv[3] ,sys.argv[4])
+elif (len(sys.argv)-1 ==5):
+    m = Master(sys.argv[1] ,sys.argv[2] ,sys.argv[3] ,sys.argv[4] , sys.argv[5])
+else:
+    m = Master()
+   
--- NodeKeeper.py
+++ NodeKeeper.py
@@ -12,43 +12,87 @@ import os
 
 
 class NodeKeeper:
-   masterPorts=["9999"]
+  #MasterPorts=["9999","9989","9979"]
+   MasterPorts=["9999"]
+  #IamAlivePorts=["9899", "9799","9699"]
+   IamAlivePorts=["9899"]
    ip = "tcp://localhost:"
-   ports = ("1300","1400","1500")
+   MyPorts = ["1300","1400","1500"]
    zmqContext = zmq.Context()
    
 
-   def __init__(self):
-      self.start()
+   def __init__(self , port1=None , port2=None , port3=None , IP=None):
+        if IP != None:
+              self.ip = IP
+        if port1 != None:
+            self.MyPorts[0] = port1
+        if port2  !=None:
+            self.MyPorts[1] = port2
+        if port3 !=None:
+            self.MyPorts[2] = port3 
+        self.start()
 
 
-
-# make 
    def start(self):
       print("NodeKeeper is Ready")
       print("waiting for Service")
-      clientSocket = self.zmqContext.socket(zmq.REP)
-      for p in self.ports:
-           clientSocket.bind("tcp://*:%s" % p)
-           
-      dataSocket = self.zmqContext.socket(zmq.REQ)
-      for p in self.masterPorts:
-          dataSocket.connect("tcp://localhost:%s" % p)
-      while True:
-          ID , FileName , operation =clientSocket.recv_pyobj()
-          print("first recieve finished")
-          clientSocket.send_string("A")
-          data = clientSocket.recv_pyobj()
-          print("second recieve finished")
-          clientSocket.send_string("A")
-          ff = open("shit.mp4", "wb")
-          ff.write(data)
-          ff.close()
-          dataSocket.send_pyobj((ID , self.ip , FileName))
-          dataSocket.recv_string()
+      t = threading.Thread(target = self.NodeAction, args = (self.MyPorts[0]))
+      t.start()
+      t1 = threading.Thread(target = self.NodeAction, args = (self.MyPorts[1]))
+      t1.start()
+      t2 = threading.Thread(target = self.NodeAction, args = (self.MyPorts[2]))
+      t2.start()
+      #t3 = threading.Thread(target = self.IamAlive, args = ())
+      #t3.start()
+      t.join()
+      t1.join()
+      t2.join()
+      #t3.join()
+      
+
+   def NodeAction(self , port):
+        ClientSocket = self.zmqContext.socket(zmq.REP)
+        ClientSocket.bind("tcp://*:%s" % port)
+        MasterSocket = self.zmqContext.socket(zmq.REQ)
+        for p in self.MasterPorts:
+          MasterSocket.connect("tcp://localhost:%s" % p)
+        while True:
+          ID , FileName , operation, index , DataOrSize =ClientSocket.recv_pyobj()
+          print("operation is " + operation)
+          if (operation =="Upload"):
+              ff = open(ID+FileName, "wb")
+              ff.write(DataOrSize)
+              ff.close()
+              print("operation is Upload done")
+              MasterSocket.send_pyobj((ID , self.ip , FileName))
+              MasterSocket.recv_string()
+              ClientSocket.send_string("Done")
+          elif(operation == "Download"):
+              ff = open(ID+FileName, "rb")
+              Data = ff.read()
+              Length = len(Data)
+              FirstPart =int ( ( Length * (index-1) ) /DataOrSize )
+              SecondPart = int ( (( Length * index ) /DataOrSize ) -1)
+              DataToSend = Data[FirstPart:SecondPart]
+              ClientSocket.send_pyobj(DataToSend)
+              MasterSocket.send_pyobj((ID , self.ip , ""))
+              MasterSocket.recv_string()
        
-    
-    
+   def IamAlive(self):
+        AliveSocket = self.zmqContext.socket(zmq.REQ)
+        for p in self.IamAlivePorts:
+          AliveSocket.connect("tcp://localhwost:%s" % p)
+        while True:
+          AliveSocket.send_pyobj((self.ip , self.MyPorts[0] , self.MyPorts[1] , self.MyPorts[2] ))
+          AliveSocket.recv_string()
+          time.sleep(1)
+          
+            
+       
 
-
-c = NodeKeeper()
+if (len(sys.argv)==4):
+    c = NodeKeeper(sys.argv[1] ,sys.argv[2] ,sys.argv[3] ,sys.argv[4])
+elif (len(sys.argv)==5):
+    c = NodeKeeper(sys.argv[1] ,sys.argv[2] ,sys.argv[3] ,sys.argv[4] , sys.argv[5] )
+else:
+    c = NodeKeeper()
